026faccb20cc5ed50915d447186b71d3eac10cbe
Myrmekes-a <nightfurry090@gmail.com>
2021-11-22 03:02:47 +0300

    Purchase Price and date

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,41 @@
 import express from 'express';
-import { fetchWalletForNFTs } from './wallet';
+import { fetchWalletForNFTs, getTransactionData } from './wallet';
 
 const app = express();
 const port = 3000;
+
 app.get('/', async (req, res) => {
-  const address = req.query.address as string;
-  console.log(`Requested wallet address ${address}`);
-  const result = await fetchWalletForNFTs(address);
-  console.log(`Request is${result == false ? ' not' : ''} process`);
-  res.send(`Requested wallet address ${address}<br>${JSON.stringify(result)}`);
+  try {
+    const address = req.query.address as string;//'9X3n2WPj8k7GB2wD7MxSxuL3VqC2e6YaafdcyPbr8xys';//
+    console.log(`Requested wallet address ${address}`);
+    const result = await fetchWalletForNFTs(address);
+    for (const nft of result ?? []) {
+      await getTransactionData(address, nft.mint);
+    }
+    console.log(`Request is${result == false ? ' not' : ''} process`);
+    res.send(`Requested wallet address ${address}<br>${JSON.stringify(result)}`);
+  } catch (e) {
+    console.log(`Request isn't process: ${e}`);
+    res.send(e);
+  }
 });
+
+app.get('/nft', async (req, res) => {
+  try {
+    const address = req.query.address as string;//'9X3n2WPj8k7GB2wD7MxSxuL3VqC2e6YaafdcyPbr8xys';
+    const mint = req.query.mint as string;//'HkaDezUF8eEHZRkDbMJGCCxNVuLuDfQ6ABpADahLw36M';
+    console.log(`Requested wallet address ${address}, mint ${mint}`);
+    const result = await getTransactionData(address, mint);
+    console.log(`Request is processed`);
+    res.send(`Requested wallet address ${address} mint ${mint}<br>${JSON.stringify(result)}`);
+  } catch (e) {
+    console.log(`Request isn't process: ${e}`);
+    res.send(e);
+  }
+});
+
 app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
+  console.log(`server is listening on ${port}`);
+  // func();
+  return ;
 });
--- src/wallet.ts
+++ src/wallet.ts
@@ -1,5 +1,5 @@
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
-import { AccountInfo, Connection, PublicKey } from "@solana/web3.js";
+import { AccountInfo, Connection, ParsedConfirmedTransaction, PublicKey } from "@solana/web3.js";
 import * as borsh from 'borsh';
 import fs from 'fs';
 import path from 'path';
@@ -8,20 +8,25 @@ import { AccountAndPubkey, Metadata, METADATA_SCHEMA } from "./types";
 
 const SOLANA_MAINNET = "https://api.mainnet-beta.solana.com";
 const DUMP_PATH = __dirname + '/../dumps';
+const DEFAULT_SOL = 1000000000;
 const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
   'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
 );
 
 export const fetchWalletForNFTs = async (address: string) => {
-    try {
+    // try {
         const wallet = new PublicKey(address);
         const connection = new Connection(SOLANA_MAINNET, "confirmed");
 
+        // Retrieve spl-tokens in wallet
         const ownedTokens = await connection.getParsedTokenAccountsByOwner(wallet, {
             programId: TOKEN_PROGRAM_ID,
         });
+
         let nftAccounts = [] as any;
         for ( const token of ownedTokens.value ) {
+
+            // Convert BN type public keys to string
             let parsedToken = {
                 account: {
                     ...token.account,
@@ -30,70 +35,195 @@ export const fetchWalletForNFTs = async (address: string) => {
                 pubkey: token.pubkey.toBase58(),
             }
             const parsedData = parsedToken.account.data.parsed;
+            // Process only nfts
             if ( parsedData.info.tokenAmount.decimals == 0
                 && parsedData.info.tokenAmount.uiAmount == 1 ) {
+                    // console.dir(parsedToken, {depth: null});
                     console.log(`--> ${nftAccounts.length + 1} nft is determinted`);
                     const metaData = await getAccountsByMint(parsedData.info.mint, connection);
                     console.log('Get token metadata performed');
                     let parsedMetaData = {} as any;
                     if ( metaData.length != 0 ) {
                         parsedMetaData = processMetaData(metaData[0][0]);
-                    }
+                    } 
                     console.log('Process token metadata performed');
+                    // console.dir(parsedMetaData, {depth: null});
                     
-                    let fetchedNFTMetadata = {};
-                    while (1) {
-                        try {
-                            const nftMetaData = await axios.get(parsedMetaData.data.uri);
-                            if (nftMetaData.status == 200) {
-                                fetchedNFTMetadata = nftMetaData.data;
-                                break;
-                            };
-                        } catch (e) {
-                            console.log(`Metadata fetch from arweave is failed. Trying again`);
-                        }
-                    }
-                    console.log('Get token nft metadata processed');
-                    const trxData = await getTransactionData(parsedData.info.mint, connection);
-                    console.log('Get related transactions processed');
                     nftAccounts.push({
                         account: token.pubkey.toBase58(),
                         mint: parsedData.info.mint,
                         metadataAccount: metaData[0][1],
                         metadata: parsedMetaData,
-                        nftMetadata: fetchedNFTMetadata,
-                        transactions: trxData,
+                        // nftMetadata: fetchedNFTMetadata,
                     })
                     console.log(nftAccounts[nftAccounts.length - 1]);
                     saveDump(
-                        `${nftAccounts.length}.json`,
+                        `${parsedData.info.mint}.json`,
                         nftAccounts[nftAccounts.length - 1]
                     );
                     console.log(`\n--> ${nftAccounts.length} Nft is processed`);
             }
         }
-        // console.dir(nftAccounts, {depth: null});
         console.log(`\n${nftAccounts.length} nfts determined from this wallet`);
 
         return nftAccounts;
-    } catch (e) {
-        console.log(`--> Error: ${e}`);
-        return false;
-    }
+    // } catch (e) {
+    //     console.log(`--> Error: ${e}`);
+    //     return false;
+    // }
 }
 
-async function getTransactionData(mint: string, connection: Connection) {
+export async function getTransactionData(address: string, mint: string) {
+    const connection = new Connection(SOLANA_MAINNET, "confirmed");
+    let dump = loadDump(`${mint}.json`);
+    if (!dump) {
+      console.log('Couldn\'t find nft metadata. Fetch NFTs and try again.');
+      return false;
+    }
+    
+    let fetchedNFTMetadata = undefined;
+    for (let again = 0; /*again < 3*/; again++) {
+        try {
+            if (!dump.metadata.data.uri) break;
+            const nftMetaData = await axios.get(dump.metadata.data.uri);
+            if (nftMetaData.status == 200) {
+                fetchedNFTMetadata = nftMetaData.data;
+                break;
+            };
+        } catch (e) {
+            console.log(`Metadata fetch from arweave is failed. Trying again`);
+        }
+    }
+    if (!fetchedNFTMetadata) {
+      console.log('Could\'t get NFT metadata. Fetch Nft again and then try again.');
+      return false;
+    }
+    console.log('Get token nft metadata processed');
+    
+
     const trxTracks = await connection.getSignaturesForAddress(new PublicKey(mint), {limit: 100}, 'confirmed');
-    let trxData = [];
-    for( const track of trxTracks) {
-        const time = track.blockTime ?? 0 * 1000;
+    console.log('--> Fetched related signature for address');
+    // console.dir(trxTracks, {depth: null});
+    let trxData = [] as any, purchasedDate = '', purchasedPrice = 0;
+    for( let idx = 0; idx < trxTracks.length; idx ++ ) {
+        const time = (trxTracks[idx].blockTime ?? 0) * 1000;
+        let date = new Date();
+        date.setTime(time);
+        if (purchasedDate == '') purchasedDate = date.toLocaleString();
         trxData.push({
-            signature: track.signature,
-            slot: track.slot,
-            blockTime: (new Date(time)).toLocaleString(),
+            signature: trxTracks[idx].signature,
+            slot: trxTracks[idx].slot,
+            blockTime: date.toLocaleString(),
         })
+        if (idx == 0 || idx == trxTracks.length - 1) {
+          const result = await getPriceInfo(trxTracks[idx].signature, address, connection);
+          purchasedPrice = result == false ? 0 : result;
+        }
     };
-    return trxData;
+
+    saveDump(`${mint}.json`, {
+      ...dump,
+      nftMetadata: fetchedNFTMetadata,
+      purchasedDate,
+      purchasedPrice,
+      transactionData: trxData,
+    });
+    
+    return {
+      purchasedPrice,
+      purchasedDate,
+      data: trxData
+    };
+}
+
+// Get Purchase Price from signature
+const getPriceInfo = async (sig: string, wallet: string, connection: Connection) => {
+    const parsedTrxDatas = await connection.getParsedConfirmedTransaction(sig, 'finalized');
+    if (parsedTrxDatas == null) return false;
+    let parsedData = parseTransactionData(parsedTrxDatas, sig);
+
+    let transaferData = [], purchaser = '', price = 0;
+    // Find mintAuthority
+    for (const ins of parsedData.transaction.message.instructions) {
+      if (ins.program == 'system' && ins.parsed.type == 'transfer') {
+        if (ins.parsed.info.lamports % 10000 != 0) continue;
+        transaferData.push(ins.parsed.info);
+      }
+      if (ins.program == 'spl-token' && ins.parsed.type == 'mintTo') {
+        purchaser = ins.parsed.info.mintAuthority;
+      }
+    }
+    for (const ins of parsedData.meta.innerInstructions) {
+      for (const innerIns of ins.instructions) {
+        if (innerIns.program == 'system' && innerIns.parsed.type == 'transfer') {
+          if (innerIns.parsed.info.lamports % 10000 != 0) continue;
+          transaferData.push(innerIns.parsed.info);
+        }
+      }
+    }
+    for (const data of transaferData)
+      if (data.source == wallet || data.source == purchaser)
+        price += data.lamports;
+    // console.dir(transaferData, {depth: null});
+    console.log(`--> Parsed price: ${purchaser} - ${price / DEFAULT_SOL}`);
+    return price;
+}
+
+// Convert all PublicKeys in the transactions data as base58 string
+export const parseTransactionData = (raw: ParsedConfirmedTransaction | null, sig: string) => {
+  let parsedData = raw as any;
+  parsedData.signature = sig;
+  // Parse innerInstruction accounts
+  let newInnerIns = [];
+  for ( const innerIns of raw?.meta?.innerInstructions ?? []) {
+      let newIns = [];
+      for ( const ins of innerIns.instructions) {
+          let newData = ins as any;
+          newData.programId = ins.programId.toBase58();
+          if (newData.accounts) {
+              let newAccounts = [];
+              for (const account of newData.accounts) newAccounts.push(account.toBase58());
+              newData.accounts = newAccounts;
+          }
+          newIns.push(newData);
+      }
+      newInnerIns.push({
+          index: innerIns.index,
+          instructions: newIns,
+      });
+  }
+  parsedData.meta.innerInstructions = newInnerIns;
+
+  // Parse transaction accounts
+  let newTransaction = raw?.transaction as any;
+  let newAccountKeys = [];
+  for ( const account of newTransaction?.message.accountKeys ?? [])
+      newAccountKeys.push({
+          ...account,
+          pubkey: account.pubkey.toBase58(),
+      })
+  let newInstructions = [];
+  for ( const ins of newTransaction?.message.instructions) {
+      let newIns = ins as any;
+      if (newIns.accounts) {
+          let newInsAccounts = [];
+          for ( const account of newIns.accounts as any) newInsAccounts.push(account.toBase58());
+          newIns.accounts = newInsAccounts;
+      }
+      newIns.programId = newIns.programId.toBase58();
+      newInstructions.push(newIns)
+  }
+  
+  newTransaction = {
+      ...newTransaction,
+      message: {
+          ...newTransaction.message,
+          instructions: newInstructions,
+          accountKeys: newAccountKeys,
+      }
+  };
+  parsedData.transaction = newTransaction;
+  return parsedData;
 }
 
 function processMetaData(meta: Metadata) {
@@ -122,6 +252,7 @@ function processMetaData(meta: Metadata) {
     return bufMeta;
 }
 
+// Get NFT Token Metadata from mint address
 async function getAccountsByMint(mint: string, connection: Connection) {
   const metadataAccounts = await getProgramAccounts(
     connection,
@@ -176,13 +307,11 @@ async function getProgramAccounts(
         }
       }
     }
-  
     const args = connection._buildArgs([programId], commitment, 'base64', extra);
     const unsafeRes = await (connection as any)._rpcRequest(
       'getProgramAccounts',
       args,
     );
-    //console.log(unsafeRes)
     const data = (
       unsafeRes.result as Array<{
         account: AccountInfo<[string, string]>;
@@ -203,40 +332,58 @@ async function getProgramAccounts(
     });
   
     return data;
-  }
-  
-  async function decodeMetadata(buffer: any) {
-    return borsh.deserializeUnchecked(METADATA_SCHEMA, Metadata, buffer);
-  }
+}
 
-  export function saveDump(
-    dumpType: string,
-    content: any,
-    cPath: string = DUMP_PATH,
-    infos: any = {},
-  ) {
-    fs.writeFileSync(
-      getDumpPath(dumpType, cPath, infos),
-      JSON.stringify(content),
-    );
+// Decode metadata from buffer with schema
+async function decodeMetadata(buffer: any) {
+  return borsh.deserializeUnchecked(METADATA_SCHEMA, Metadata, buffer);
+}
+
+export function saveDump(
+  dumpType: string,
+  content: any,
+  cPath: string = DUMP_PATH,
+  infos: any = {},
+) {
+  fs.writeFileSync(
+    getDumpPath(dumpType, cPath, infos),
+    JSON.stringify(content),
+  );
+}
+
+/**
+ * Restore dump content as file
+ * 
+ * @param dumpType Type of dump which is used to resolve dump file name
+ * @param cPath Location of saved dump file
+ * @returns JSON object or undefined
+ */
+ export function loadDump(
+  dumpType: string,
+  cPath: string = DUMP_PATH,
+) {
+  const path = getDumpPath(dumpType, cPath);
+  return fs.existsSync(path)
+    ? JSON.parse(fs.readFileSync(path).toString())
+    : undefined;
+}
+
+/**
+ * Resolve dump file path from dumpType
+ * 
+ * @param dumpType Type of dump which is used to resolve dump file name
+ * @param cPath Location of saved dump file
+ * @param infos Optional param for track transactions. Save period info in the dump file name
+ * @returns Location of subdirectory of exact dump file
+ */
+export function getDumpPath(
+  dumpType: string,
+  cPath: string = DUMP_PATH,
+  infos: any = {},
+) {
+  if (!fs.existsSync(cPath)) fs.mkdirSync(cPath, {recursive: true});
+  switch (dumpType) {
+    default:
+      return path.join(cPath, dumpType);
   }
-  
-  /**
-   * Resolve dump file path from dumpType
-   * 
-   * @param dumpType Type of dump which is used to resolve dump file name
-   * @param cPath Location of saved dump file
-   * @param infos Optional param for track transactions. Save period info in the dump file name
-   * @returns Location of subdirectory of exact dump file
-   */
-  export function getDumpPath(
-    dumpType: string,
-    cPath: string = DUMP_PATH,
-    infos: any = {},
-  ) {
-    if (!fs.existsSync(cPath)) fs.mkdirSync(cPath, {recursive: true});
-    switch (dumpType) {
-      default:
-        return path.join(cPath, dumpType);
-    }
-  }
+}
